c2a0fd75e4df78df0ce6cdf57aec2ee8d491c184
Alex Petrosh <petrosh@users.noreply.github.com>
2017-12-22 17:53:02 +0100

    Smooth change of background: HTML.yellow 5 seconds

--- script2.js
+++ script2.js
@@ -43,50 +43,52 @@ $(document).ready(function() {
 		// console.log('data',$(data).find("item"));
 		//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
 		$(data).find("item").each(function () { // or "item" or whatever suits your feed
-				var el = $(this), cat = [];
-				var entry = document.importNode(document.querySelector('#article').content, true);
-				var link = el.find("link").text();
-				// Title with link
-				entry.querySelector('h2 a').innerHTML = el.find("title").text();
-				entry.querySelector('h2 a').href = link;
-				// Set content
-				// var content = el.find("description").text().replace(/<p><br \/><b><a href=\"(.*?)\">Leggi e commenta il post <\/a> su www.beppegrillo.it<\/b><\/p>/g,'');
-				var content = el.find("description")
-					.html()
-					.replace(/<!--\[CDATA\[<p-->/g,'')
-					.replace(/">Leggi e commenta il post <\/a> su www.beppegrillo.it<\/b><\/p>\]\]&gt;/g,'</span>')
-					.replace(/<p><br><b><a href="/g,'<span class="final-link">')
-					.replace(/<p><br \/><b><a href="(.*?)">Leggi e commenta il post <\/a> su www.beppegrillo.it<\/b><\/p>]]>/g,'');
-				entry.querySelector('div').innerHTML = content;
-				// Set date
-				var date = new Date(el.find("pubDate").text()).toLocaleDateString("it-IT", {
-					weekday: "long",
-					day: "numeric",
-					month: "long",
-					year: "numeric",
-					hour: "numeric",
-					minute: "numeric"
-				}).toUpperCase();
-				entry.querySelector('small').innerHTML = date;
-				// Set categories
-				el.find("category").map( function (i,c) {
-					entry.querySelector('header small').innerHTML += ((i !== 0) ? ', ' : '') + c.innerHTML;
-				});
-				// Check if article is viewed
-				if (viewed.indexOf(el.find("link").text()) === -1) {
-					// Not viewed
-					// viewed = viewed.slice(0, 10); // end is not included
-					// Add new element
-					viewed[viewed.length] = el.find("link").text();
-					// limit array, strip firsts
-					if(viewed.length>15){
-						viewed = viewed.slice((viewed.length-15), viewed.length);
-					}
-					// Apply new class
-					entry.querySelector('small').className += ' new';
+			var el = $(this), cat = [];
+			var entry = document.importNode(document.querySelector('#article').content, true);
+			var link = el.find("link").text();
+			// Title with link
+			entry.querySelector('h2 a').innerHTML = el.find("title").text();
+			entry.querySelector('h2 a').href = link;
+			// Set content
+			// var content = el.find("description").text().replace(/<p><br \/><b><a href=\"(.*?)\">Leggi e commenta il post <\/a> su www.beppegrillo.it<\/b><\/p>/g,'');
+			var content = el.find("description")
+				.html()
+				.replace(/<!--\[CDATA\[<p-->/g,'')
+				.replace(/">Leggi e commenta il post <\/a> su www.beppegrillo.it<\/b><\/p>\]\]&gt;/g,'</span>')
+				.replace(/<p><br><b><a href="/g,'<span class="final-link">')
+				.replace(/<p><br \/><b><a href="(.*?)">Leggi e commenta il post <\/a> su www.beppegrillo.it<\/b><\/p>]]>/g,'');
+			entry.querySelector('div').innerHTML = content;
+			// Set date
+			var date = new Date(el.find("pubDate").text()).toLocaleDateString("it-IT", {
+				weekday: "long",
+				day: "numeric",
+				month: "long",
+				year: "numeric",
+				hour: "numeric",
+				minute: "numeric"
+			}).toUpperCase();
+			entry.querySelector('small').innerHTML = date;
+			// Set categories
+			el.find("category").map( function (i,c) {
+				entry.querySelector('header small').innerHTML += ((i !== 0) ? ', ' : '') + c.innerHTML;
+			});
+			// Check if article is viewed
+			if (viewed.indexOf(el.find("link").text()) === -1) {
+				$('html:not(.yellow)').addClass('yellow');
+				// Not viewed
+				// viewed = viewed.slice(0, 10); // end is not included
+				// Add new element
+				viewed[viewed.length] = el.find("link").text();
+				// limit array, strip firsts
+				if(viewed.length>15){
+					viewed = viewed.slice((viewed.length-15), viewed.length);
 				}
-				// Add to DOM
-				document.querySelector("section").appendChild(entry);
+				// Apply new class
+				entry.querySelector('small').className += ' new';
+			}
+			
+			// Add to DOM
+			document.querySelector("section").appendChild(entry);
 		}).promise().done( function(){
 			// store viewed
 			localStorage.setItem('viewedFinal', JSON.stringify(viewed));
